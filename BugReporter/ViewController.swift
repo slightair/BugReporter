57db212f2e5ed80ceb9249959dc769a31678f98d
slightair <arksutite@gmail.com>
2016-03-03 14:33:07 +0900

    Create project

---
+++ BugReporter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BugReporter
+//
+//  Created by tomohiro-moro on 3/3/16.
+//  Copyright © 2016 slightair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
